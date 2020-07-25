fe81696b30c8dd89aea4756dff16f4c5582ed63f
Yitzchak Schwarz <s.y.schwarz@gmail.com>
2020-07-26 00:39:42 +0300

    Basic Layout

--- practical-react/src/App.js
+++ practical-react/src/App.js
@@ -3,24 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Cars />
+			<Footer />
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<header>Header</header>
+	);
+}
+
+function Cars() {
+	return (
+		<main>
+			Cars
+		</main>
+	);
+}
+
+function Footer() {
+	return (
+		<footer>Footer</footer>
+	);
 }
 
 export default App;
