5e08d1f78ce3843bee9644cb7428d9bfdea54d34
odeyateherani <odeyalosh123@gmail.com>
2020-07-29 21:39:50 +0300

    add package

--- practical-react/src/painting-extra-details/Painting-extra-details.js
+++ practical-react/src/painting-extra-details/Painting-extra-details.js
@@ -15,12 +15,15 @@ class PaintingExtraDetails extends React.Component {
                         <img className="img" src={this.props.data.img} alt=""/>
                     </div>
 
-                    <div class="middle-card">
-                        <p className="text-color"><h4><b>Name Of The Painting:</b></h4> <h5>{this.props.data.title}</h5></p>
-                        <p className="text-color"><h4><b>Artist:</b></h4> <h5>{this.props.data.artist}</h5></p>
-                        <p className="text-color"><h4><b>Description:</b></h4> <h5>{this.props.data.description}</h5></p>
+                    <div className="middle-card">
+                        <h4>  <p className="text-color"><b>Name Of The Painting:</b></p></h4>
+                        <h5>  <p className="text-color"> {this.props.data.title}</p></h5>
+                        <h4> <p className="text-color"><b>Artist:</b></p></h4>
+                        <h5> <p className="text-color">{this.props.data.artist}</p></h5>
+                        <h4> <p className="text-color"><b>Description:</b></p></h4>
+                        <h5> <p className="text-color">{this.props.data.description}</p></h5>
                         <br/>
-                        <button className="btn btn-info">Buy Now</button>
+                        <button className="btn btn-info btn-size">Buy Now</button>
                     </div>
                 </div>
             </div>
