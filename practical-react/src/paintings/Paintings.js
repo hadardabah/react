ba7a4837da3cfb281e73c561bc0bac7b78d629cb
odeyaTeherani <odeyalosh123@gmail.com>
2020-07-30 13:23:37 +0300

    install droupdown

--- practical-react/src/paintings/Paintings.js
+++ practical-react/src/paintings/Paintings.js
@@ -3,6 +3,8 @@ import './Paintings.scss';
 import '.././painting-extra-details/Painting-extra-details.js'
 import PaintingExtraDetails from "../painting-extra-details/Painting-extra-details";
 import {string} from "prop-types";
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 class Paintings extends React.Component {
 
@@ -19,18 +21,27 @@ class Paintings extends React.Component {
     };
 
     render() {
+        this.SearchByCategory();
         return (
             <div>
-                {this.state.flag === true ? <PaintingExtraDetails data={this.state.extra_details_paint}/> : this.List()}
+                <div>
+                    <DropdownButton id="search-button" title="Search By Category">
+                        <Dropdown.Item href="#/action-1">Famous Paintings</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Animals</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Digital Art</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4">Landscape Paintings</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+                <div>
+                    {this.state.flag === true ?
+                        <PaintingExtraDetails data={this.state.extra_details_paint}/> : this.List()}
+                </div>
             </div>
         );
     }
 
     List() {
         if (this.props.data) {
-            // if(des.length > 20)
-            //     this.prop.data.description = des.substr(0,20) + '...';
-            // console.log(this.props.data);
             return (
                 this.props.data.map((paint) => (
                         <div style={{display: "inline-block"}}>
@@ -42,6 +53,17 @@ class Paintings extends React.Component {
         } else {
             return (<p>DB Empty</p>)
         }
+    }
+
+    SearchByCategory() {
+        return (
+            <DropdownButton id="search-button" title="Search By Category">
+                <Dropdown.Item href="#/action-1">Famous Paintings</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Animals</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Digital Art</Dropdown.Item>
+                <Dropdown.Item href="#/action-4">Landscape Paintings</Dropdown.Item>
+            </DropdownButton>
+        )
     }
 
     Card(paint) {
