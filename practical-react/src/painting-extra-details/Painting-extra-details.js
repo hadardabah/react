5b6aa8d6af75ac4d294190f62c4553b269854243
Yitzchak Schwarz <s.y.schwarz@gmail.com>
2020-07-30 17:59:13 +0300

    Adaptive design for details view
    Updated to look like the list view

--- practical-react/src/painting-extra-details/Painting-extra-details.js
+++ practical-react/src/painting-extra-details/Painting-extra-details.js
@@ -9,24 +9,19 @@ class PaintingExtraDetails extends React.Component {
 
     Paint() {
         return (
-            <div style={{display: "inline-block"}}>
-                <div className="paint">
-                    <div>
-                        <img className="img" src={this.props.data.img} alt=""/>
-                    </div>
+			<div className="paint">
+				<img className="img" src={this.props.data.img} alt=""/>
 
-                    <div className="middle-card">
-                        <h4>  <p className="text-color"><b>Name Of The Painting:</b></p></h4>
-                        <h5>  <p className="text-color"> {this.props.data.title}</p></h5>
-                        <h4> <p className="text-color"><b>Artist:</b></p></h4>
-                        <h5> <p className="text-color">{this.props.data.artist}</p></h5>
-                        <h4> <p className="text-color"><b>Description:</b></p></h4>
-                        <h5> <p className="text-color">{this.props.data.description}</p></h5>
-                        <br/>
-                        <button className="btn btn-info btn-size">Buy Now</button>
-                    </div>
-                </div>
-            </div>
+				<div className="middle-card">
+					<h2>{this.props.data.title}</h2>
+					<h5>Artist:</h5>
+					<p>{this.props.data.artist}</p>
+					<h5>Description:</h5>
+					<p>{this.props.data.description}</p>
+					<br/>
+					<button className="btn btn-info">Buy Now</button>
+				</div>
+			</div>
         )
     }
 }
